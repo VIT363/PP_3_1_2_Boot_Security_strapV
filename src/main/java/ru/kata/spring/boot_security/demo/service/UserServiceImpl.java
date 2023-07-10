8ae37dd651a66af9d5a247b657c34e3ae6f85feb
VIT363 <mexx-363@yandex.ru>
2023-07-10 11:36:24 +0300

    +strap

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -0,0 +1,89 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public User findByUsername(String email) {
+        return userRepository.findByUsername(email);
+    }
+
+    @Override
+    @Transactional
+    public void saveUser(User user) {
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public void adminRedactor(User user, Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            User editUser = optionalUser.get();
+            editUser.setId(user.getId());
+            editUser.setName(user.getName());
+            editUser.setSurname(user.getSurname());
+            editUser.setEmail(user.getEmail());
+            editUser.setRoles(user.getRoles());
+            if (!editUser.getPassword().equals(user.getPassword())) {
+                editUser.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+            }
+            userRepository.save(editUser);
+        }
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public User getUserByEmail(String email) {
+        return userRepository.getUserByEmail(email);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        userRepository.delete(userRepository.getById(id));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = findByUsername(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return user;
+    }
+
+}
