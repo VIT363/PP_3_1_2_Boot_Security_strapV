3ba8a27b9393569fd559dcf6bb2e65a50d1c5ca2
VIT363 <mexx-363@yandex.ru>
2023-07-10 18:35:06 +0300

    поправки

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -1,12 +1,11 @@
 package ru.kata.spring.boot_security.demo.service;
-
 import org.springframework.security.core.userdetails.UserDetailsService;
 import ru.kata.spring.boot_security.demo.model.User;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface UserService extends UserDetailsService {
+public interface UserService  {
 
     void delete(Long id);
     User findByUsername(String email);
@@ -20,3 +19,4 @@ public interface UserService extends UserDetailsService {
     void adminRedactor(User user, Long id);
     User getUserByEmail(String email);
 }
+
